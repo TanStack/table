0e9a4ec9d2b685cae66afa2e0e094a0b6b1b1f63
kieronsutton00 <kieron.sutton99@outlook.com>
2017-06-20 11:27:40 +0100

    Update defaultProps.js
    A more reliable defaultSortMethod. Works with uppercase/lowercase, negative to positive numbers, etc.

--- src/defaultProps.js
+++ src/defaultProps.js
@@ -36,21 +36,7 @@ export default {
       : true
   },
   defaultSortMethod: (a, b) => {
-    // force null and undefined to the bottom
-    a = a === null || a === undefined ? '' : a
-    b = b === null || b === undefined ? '' : b
-    // force any string values to lowercase
-    a = typeof a === 'string' ? a.toLowerCase() : a
-    b = typeof b === 'string' ? b.toLowerCase() : b
-    // Return either 1 or -1 to indicate a sort priority
-    if (a > b) {
-      return 1
-    }
-    if (a < b) {
-      return -1
-    }
-    // returning 0, undefined or any falsey value will use subsequent sorts or the index as a tiebreaker
-    return 0
+    return a - b;
   },
 
   // Controlled State Props
