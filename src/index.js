2ecf40518e794cf1581126ed98be0ca281604558
Maksim Horbachevsky <fantactuka@gmail.com>
2019-07-23 14:03:17 -0400

    Export utils to create custom hooks (#1403)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+import * as utils from './utils';
+export { utils };
 export { useTable } from './hooks/useTable'
 export { useColumns } from './hooks/useColumns'
 export { useRows } from './hooks/useRows'
-export { useTableState } from './hooks/useTableState'
+export { useTableState, defaultState } from './hooks/useTableState'
 export { useExpanded } from './plugin-hooks/useExpanded'
 export { useFilters } from './plugin-hooks/useFilters'
 export { useGroupBy } from './plugin-hooks/useGroupBy'
@@ -9,4 +11,4 @@ export { useSortBy } from './plugin-hooks/useSortBy'
 export { usePagination } from './plugin-hooks/usePagination'
 export { useFlexLayout } from './plugin-hooks/useFlexLayout'
 export { useTokenPagination } from './plugin-hooks/useTokenPagination'
-export { actions } from './actions'
+export { actions, addActions } from './actions'
