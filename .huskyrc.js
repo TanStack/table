d7a4db0979ceb6f4efb0a23c783c9ff8e4656c45
Larry Botha <larry@fixate.it>
2019-07-30 00:45:46 -0500

    chore(commitlint): add deps, config, and git hook (#1419)

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   hooks: {
     'pre-commit': 'lint-staged',
-    // 'prepare-commit-msg': 'exec < /dev/tty && git cz --hook',
+    'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
   },
 }
