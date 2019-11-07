c943ab2ddd1661e3bddf3798bc09a553a89c7d92
gargroh <42495927+gargroh@users.noreply.github.com>
2019-11-07 13:14:06 +0530

    Update useColumnVisibility.js

--- src/plugin-hooks/useColumnVisibility.js
+++ src/plugin-hooks/useColumnVisibility.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { defaultState } from '..//hooks/useTable';
+import { defaultState } from '../hooks/useTable';
 import { addActions, actions } from '../actions';
 import { determineHeaderVisibility } from '../utils';
 
