1be9f59180d3c8fd6c198029246f999a9d3e718d
Nicolas Beauvais <2951704+nicolasbeauvais@users.noreply.github.com>
2019-11-20 16:00:07 +0100

    Fix typo (#1645)

--- examples/sub-components/src/App.js
+++ examples/sub-components/src/App.js
@@ -85,14 +85,14 @@ function Table({ columns: userColumns, data, renderRowSubComponent }) {
                   </tr>
                   {/*
                     If the row is in an expanded state, render a row with a
-                    column that spands the entire length of the table.
+                    column that fills the entire length of the table.
                   */}
                   {row.isExpanded ? (
                     <tr>
                       <td colSpan={flatColumns.length}>
                         {/*
                           Inside it, call our renderRowSubComponent function. In reality,
-                          you coul pass whatever you want as props to
+                          you could pass whatever you want as props to
                           a component like this, including the entire
                           table instance. But for this example, we'll just
                           pass the row
