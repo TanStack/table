c740a0c7af3ad2bb717e63b0cdcd9769ce9ec07b
Tanner Linsley <tannerlinsley@gmail.com>
2020-03-06 14:19:15 -0700

    Github action for size tracking, better imports and building

--- rollup.config.js
+++ rollup.config.js
@@ -1,50 +1,44 @@
 import babel from 'rollup-plugin-babel'
-import commonjs from 'rollup-plugin-commonjs'
-import external from 'rollup-plugin-peer-deps-external'
-import resolve from 'rollup-plugin-node-resolve'
-import { sizeSnapshot } from 'rollup-plugin-size-snapshot'
+import { terser } from 'rollup-plugin-terser'
 import size from 'rollup-plugin-size'
-import pkg from './package.json'
+import externalDeps from 'rollup-plugin-peer-deps-external'
+
+const external = ['react']
+
+const globals = {
+  react: 'React',
+}
 
 export default [
   {
     input: 'src/index.js',
     output: {
-      file: pkg.main,
-      format: 'cjs',
+      name: 'ReactQuery',
+      file: 'dist/react-table.development.js',
+      format: 'umd',
       sourcemap: true,
+      globals,
     },
-    plugins: [
-      external(),
-      babel(),
-      resolve(),
-      commonjs(),
-      size({
-        publish: true,
-        exclude: pkg.module,
-        filename: 'sizes-cjs.json',
-        writeFile: process.env.CI ? true : false,
-      }),
-      sizeSnapshot(),
-    ],
+    external,
+    plugins: [babel(), externalDeps()],
   },
   {
     input: 'src/index.js',
     output: {
-      file: pkg.module,
-      format: 'es',
+      name: 'ReactQuery',
+      file: 'dist/react-table.production.min.js',
+      format: 'umd',
       sourcemap: true,
+      globals,
     },
+    external,
     plugins: [
-      external(),
       babel(),
+      externalDeps(),
+      terser(),
       size({
-        publish: true,
-        exclude: pkg.main,
-        filename: 'sizes-es.json',
-        writeFile: process.env.CI ? true : false,
+        writeFile: false,
       }),
-      sizeSnapshot(),
     ],
   },
 ]
