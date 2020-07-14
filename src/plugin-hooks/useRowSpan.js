171cb607074a0da058af627db6f021450934ca34
Nick Levesque <nick@levesque.io>
2020-07-14 14:54:06 -0400

    feat(useRowSpan): add plugin for rowSpan

---
+++ src/plugin-hooks/useRowSpan.js
@@ -0,0 +1,67 @@
+import React from 'react'
+
+import {
+  ensurePluginOrder,
+  useGetLatest,
+} from '../publicUtils'
+
+export const useRowSpan = hooks => {
+  hooks.useInstance.push(useInstance)
+  hooks.prepareRow.push(prepareRow)
+}
+
+useRowSpan.pluginName = 'useRowSpan'
+
+function useInstance(instance) {
+  const {
+    data,
+    rows,
+    allColumns,
+    plugins,
+    state,
+    getHooks,
+    rowSpanEnabled = true,
+    rowSpanHierarchy = true
+  } = instance
+  ensurePluginOrder(plugins, ['useFilters', 'useSortBy'], 'useRowSpan')
+  const getInstance = useGetLatest(instance)
+  
+  const { allColumns } = getInstance();
+    
+  allColumns.forEach(column => {
+    if (column.enableRowSpan) {
+      column.topCellValue = null;
+      column.topCellIndex = 0;
+    }
+  })
+
+  function prepareRow(row) {
+    let numCells = row.allCells.length;
+
+    let parentBoundary = false;
+    for (let j = 0; j < numCells; j++) {
+      let cell = row.allCells[j];
+      let column = cell.column;
+
+      if (rowSpanEnabled && column.enableRowSpan) {
+        if (
+          column.topCellValue !== cell.value // we have a non-duplicate cell
+            || cell.value === ""             // or we have a blank cell
+            || (rowSpanHierarchy && parentBoundary) // or boundary crossed
+            || column.topCellValue === null // or we are on the first row
+        ) { // this is a top cell.
+          column.topCellValue = cell.value;
+          parentBoundary = true;
+          cell.spannedRows = [row];
+          cell.rowSpan = 1;
+          cell.isRowSpanned = false;
+        } else { // cell is a duplicate and should be row-spanned.
+          cell.isRowSpanned = true;
+          // update the top cell. need to reach back in the array.
+          rows[column.topCellIndex].allCells[j].rowSpan++;
+          rows[column.topCellIndex].allCells[j].spannedRows.push(row)
+        }
+      } // else rowspan disabled for this cell - do nothing.
+    }
+  }
+}
