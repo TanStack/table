568150917e346449d7523797fde9200ecf74168a
Nick Levesque <nick@levesque.io>
2020-07-14 16:10:47 -0400

    feat(useRowSpan): move prepareRow definition outside of useInstance

--- src/plugin-hooks/useRowSpan.js
+++ src/plugin-hooks/useRowSpan.js
@@ -32,34 +32,34 @@ function useInstance(instance) {
       column.topCellIndex = 0;
     }
   })
+}
 
-  function prepareRow(row) {
-    let numCells = row.allCells.length;
-
-    let parentBoundary = false;
-    for (let j = 0; j < numCells; j++) {
-      let cell = row.allCells[j];
-      let column = cell.column;
-
-      if (rowSpanEnabled && column.enableRowSpan) {
-        if (
-          column.topCellValue !== cell.value // we have a non-duplicate cell
-            || cell.value === ""             // or we have a blank cell
-            || (rowSpanHierarchy && parentBoundary) // or boundary crossed
-            || column.topCellValue === null // or we are on the first row
-        ) { // this is a top cell.
-          column.topCellValue = cell.value;
-          parentBoundary = true;
-          cell.spannedRows = [row];
-          cell.rowSpan = 1;
-          cell.isRowSpanned = false;
-        } else { // cell is a duplicate and should be row-spanned.
-          cell.isRowSpanned = true;
-          // update the top cell. need to reach back in the array.
-          rows[column.topCellIndex].allCells[j].rowSpan++;
-          rows[column.topCellIndex].allCells[j].spannedRows.push(row)
-        }
-      } // else rowspan disabled for this cell - do nothing.
-    }
+function prepareRow(row) {
+  let numCells = row.allCells.length;
+  
+  let parentBoundary = false;
+  for (let j = 0; j < numCells; j++) {
+    let cell = row.allCells[j];
+    let column = cell.column;
+    
+    if (rowSpanEnabled && column.enableRowSpan) {
+      if (
+        column.topCellValue !== cell.value // we have a non-duplicate cell
+          || cell.value === ""             // or we have a blank cell
+          || (rowSpanHierarchy && parentBoundary) // or boundary crossed
+          || column.topCellValue === null // or we are on the first row
+      ) { // this is a top cell.
+        column.topCellValue = cell.value;
+        parentBoundary = true;
+        cell.spannedRows = [row];
+        cell.rowSpan = 1;
+        cell.isRowSpanned = false;
+      } else { // cell is a duplicate and should be row-spanned.
+        cell.isRowSpanned = true;
+        // update the top cell. need to reach back in the array.
+        rows[column.topCellIndex].allCells[j].rowSpan++;
+        rows[column.topCellIndex].allCells[j].spannedRows.push(row)
+      }
+    } // else rowspan disabled for this cell - do nothing.
   }
 }
