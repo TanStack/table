f138292c120e46783470eec03b2d5cd75987ac2b
Anat Sheba Harari <harari.anat@gmail.com>
2020-10-04 22:31:43 +0300

    fix: pagination on sort (#2669)

--- src/pagination.js
+++ src/pagination.js
@@ -65,8 +65,8 @@ export default class ReactTablePagination extends Component {
   }
 
   componentDidUpdate (prevProps, prevState) {
-    if (prevProps.page !== this.props.page && prevState.page !== this.state.page) {
-      // this is probably safe because we only update when old/new state.page are different
+    if (prevProps.page !== this.props.page || prevState.page !== this.state.page) {
+      // this is probably safe because we only update when old/new props/state.page are different
       // eslint-disable-next-line react/no-did-update-set-state
       this.setState({
         page: this.props.page,
