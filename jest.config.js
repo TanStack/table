00d6322ad139b0872fa66c23791f8afebb2156cb
goatfryed <omar.sood@onquality.de>
2020-10-24 17:24:34 +0200

    update jest test file inclusion

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   collectCoverage: true,
   coverageReporters: ['json', 'lcov', 'text', 'clover', 'text-summary'],
   setupFilesAfterEnv: ['./jest.setup.js'],
-  testMatch: ['<rootDir>/src/**/*.test.tsx'],
+  testRegex: ['/src/.*\.test\.(t|j)sx?'],
   testPathIgnorePatterns: ['<rootDir>/types/'],
   moduleNameMapper: {
     'react-table': '<rootDir>/src/index.ts',
