d0f1c3a79c0d71dc3217ec0c5ddbf3295066416b
Anthony Garand <garand@garand.me>
2021-06-07 10:09:00 -0400

    fix(deps): include babel runtime in build
    This updates the Rollup Babel plugin and also includes the Babel runtime in the final build, as recommended by Babel. The Rollup Babel plugin has babelHelpers set to "runtime", which is recommended for libraries in the plugin documentation.
    
    https://www.npmjs.com/package/@rollup/plugin-babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import babel from 'rollup-plugin-babel'
+import { babel } from '@rollup/plugin-babel'
 import { terser } from 'rollup-plugin-terser'
 import size from 'rollup-plugin-size'
 import externalDeps from 'rollup-plugin-peer-deps-external'
@@ -23,7 +23,7 @@ export default [
     external,
     plugins: [
       replace({ 'process.env.NODE_ENV': `"development"`, delimiters: ['', ''] }),
-      babel(),
+      babel({ babelHelpers: 'runtime' }),
       externalDeps(),
     ],
   },
@@ -39,7 +39,7 @@ export default [
     external,
     plugins: [
       replace({ 'process.env.NODE_ENV': `"production"`, delimiters: ['', ''] }),
-      babel(),
+      babel({ babelHelpers: 'runtime' }),
       externalDeps(),
       terser(),
       size({
