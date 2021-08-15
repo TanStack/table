6e9cbc2f2019dc756d125e6cb928858658618c2d
Nirojan Selvanathan <sshniro@gmail.com>
2021-08-15 16:46:55 +0200

    Fix for page index not resetting for each API call
    # Description
    - Based on the above example fetchData will be called twice, and during the second call, the pageIndex will be set to zero.
    - In order to prevent resetting of the tabledata, the `autoResetPage: false` should be set to simulate proper server-side rendering.
    - Related issue: #411

--- examples/pagination-controlled/src/App.js
+++ examples/pagination-controlled/src/App.js
@@ -73,6 +73,7 @@ function Table({
       // This means we'll also have to provide our own
       // pageCount.
       pageCount: controlledPageCount,
+      autoResetPage: false,
     },
     usePagination
   )
