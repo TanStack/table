8bfd2b6a6ef7174cd2b0c682570409706cf47a26
Tanner Linsley <tannerlinsley@gmail.com>
2022-05-05 20:22:57 -0600

    Update getFilteredRowModelAsync.ts

--- packages/table-core/src/utils/getFilteredRowModelAsync.ts
+++ packages/table-core/src/utils/getFilteredRowModelAsync.ts
@@ -1,5 +1,7 @@
+import { ResolvedColumnFilter } from '../features/Filters'
 import { TableInstance, RowModel, TableGenerics, Row } from '../types'
 import { incrementalMemo } from '../utils'
+import { filterRows } from './filterRowsUtils'
 
 export function getFilteredRowModelAsync<
   TGenerics extends TableGenerics
@@ -9,24 +11,130 @@ export function getFilteredRowModelAsync<
   return instance =>
     incrementalMemo(
       () => [
-        instance.getState().columnFilters,
         instance.getPreFilteredRowModel(),
+        instance.getState().columnFilters,
+        instance.getState().globalFilter,
       ],
-      (_sorting, rowModel): RowModel<TGenerics> => {
+      (rowModel): RowModel<TGenerics> => {
         return {
           rows: rowModel.rows.slice(),
           flatRows: [],
           rowsById: rowModel.rowsById,
         }
       },
-      (columnFilters, rowModel) => rowModelRef => scheduleTask => {
-        // TODO: Figure out how to do scheduled filtering
-        // The trick will be batching those tasks in a way
-        // that makes them fast. JS work loops don't tend to do well with
-        // a high number of tasks that do one thing. Instead, shoot for each
-        // task to have a few thousand opts (or whatever amount is generally
-        // fast for most devices).
-        throw new Error('')
+      (rowModel, columnFilters, globalFilter) => rowModelRef => scheduleTask => {
+        if (
+          !rowModel.rows.length ||
+          (!columnFilters?.length && !globalFilter)
+        ) {
+          rowModelRef.current = rowModel
+          return
+        }
+
+        const resolvedColumnFilters: ResolvedColumnFilter<TGenerics>[] = []
+        const resolvedGlobalFilters: ResolvedColumnFilter<TGenerics>[] = []
+
+        ;(columnFilters ?? []).forEach(d => {
+          const column = instance.getColumn(d.id)
+
+          if (!column) {
+            if (process.env.NODE_ENV !== 'production') {
+              console.warn(
+                `Table: Could not find a column to filter with columnId: ${d.id}`
+              )
+            }
+          }
+
+          const filterFn = column.getFilterFn()!
+
+          if (!filterFn) {
+            if (process.env.NODE_ENV !== 'production') {
+              console.warn(
+                `Could not find a valid 'column.filterFn' for column with the ID: ${column.id}.`
+              )
+            }
+            return
+          }
+
+          resolvedColumnFilters.push({
+            id: d.id,
+            filterFn,
+            resolvedValue: filterFn.resolveFilterValue?.(d.value) ?? d.value,
+          })
+        })
+
+        const filterableIds = columnFilters.map(d => d.id)
+
+        const globalFilterFn = instance.getGlobalFilterFn()
+
+        const globallyFilterableColumns = instance
+          .getAllLeafColumns()
+          .filter(column => column.getCanGlobalFilter())
+
+        if (
+          globalFilter &&
+          globalFilterFn &&
+          globallyFilterableColumns.length
+        ) {
+          filterableIds.push('__global__')
+
+          globallyFilterableColumns.forEach(column => {
+            resolvedGlobalFilters.push({
+              id: column.id,
+              filterFn: globalFilterFn,
+              resolvedValue:
+                globalFilterFn.resolveFilterValue?.(globalFilter) ??
+                globalFilter,
+            })
+          })
+        }
+
+        // Flag the prefiltered row model with each filter state
+        for (let j = 0; j < rowModel.flatRows.length; j++) {
+          const row = rowModel.flatRows[j]
+
+          row.columnFilterMap = {}
+
+          if (resolvedColumnFilters.length) {
+            for (let i = 0; i < resolvedColumnFilters.length; i++) {
+              const columnFilter = resolvedColumnFilters[i]
+
+              // Tag the row with the column filter state
+              row.columnFilterMap[columnFilter.id] = columnFilter.filterFn(
+                row,
+                columnFilter.id,
+                columnFilter.resolvedValue
+              )
+            }
+          }
+
+          if (resolvedGlobalFilters.length) {
+            let hit = false
+
+            for (let i = 0; i < resolvedGlobalFilters.length; i++) {
+              const globalFilter = resolvedGlobalFilters[i]
+              // Tag the row with the first truthy global filter state
+              if (
+                globalFilter.filterFn(
+                  row,
+                  globalFilter.id,
+                  globalFilter.resolvedValue
+                )
+              ) {
+                hit = true
+                row.columnFilterMap.__global__ = true
+                break
+              }
+            }
+
+            if (row.columnFilterMap.__global__ !== true) {
+              row.columnFilterMap.__global__ = false
+            }
+          }
+        }
+
+        // Filter final rows using all of the active filters
+        return filterRows(rowModel.rows, filterableIds, instance)
       },
       {
         key:
