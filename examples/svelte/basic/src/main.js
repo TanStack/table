c539e481309a11a77480338db3f897ff357baba7
Tanner Linsley <tannerlinsley@gmail.com>
2022-05-19 09:16:12 -0600

    exmples: remove generated files

--- examples/svelte/basic/src/main.js
+++
@@ -1,6 +0,0 @@
-import App from './App.svelte';
-const app = new App({
-    target: document.getElementById('root'),
-});
-export default app;
-//# sourceMappingURL=main.js.map
--- examples/svelte/sorting/src/main.d.ts
+++
@@ -1,3 +0,0 @@
-import App from './App.svelte';
-declare const app: App;
-export default app;
