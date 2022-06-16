d6064eb66c80140494b62869e9a4763b1b60a1fb
Tanner Linsley <tannerlinsley@gmail.com>
2022-06-17 00:20:32 +0200

    fix: filterFns check for null
    Fixes #4008

--- packages/table-core/src/filterFns.ts
+++ packages/table-core/src/filterFns.ts
@@ -6,7 +6,7 @@ const includesString: FilterFn<any> = (
   filterValue: string
 ) => {
   const search = filterValue.toLowerCase()
-  return row.getValue(columnId).toLowerCase().includes(search)
+  return row.getValue(columnId)?.toLowerCase().includes(search)
 }
 
 includesString.autoRemove = (val: any) => testFalsey(val)
@@ -16,7 +16,7 @@ const includesStringSensitive: FilterFn<any> = (
   columnId: string,
   filterValue: string
 ) => {
-  return row.getValue(columnId).includes(filterValue)
+  return row.getValue(columnId)?.includes(filterValue)
 }
 
 includesStringSensitive.autoRemove = (val: any) => testFalsey(val)
@@ -26,7 +26,7 @@ const equalsString: FilterFn<any> = (
   columnId: string,
   filterValue: string
 ) => {
-  return row.getValue(columnId).toLowerCase() === filterValue.toLowerCase()
+  return row.getValue(columnId)?.toLowerCase() === filterValue.toLowerCase()
 }
 
 equalsString.autoRemove = (val: any) => testFalsey(val)
@@ -36,7 +36,7 @@ const arrIncludes: FilterFn<any> = (
   columnId: string,
   filterValue: unknown
 ) => {
-  return row.getValue(columnId).includes(filterValue)
+  return row.getValue(columnId)?.includes(filterValue)
 }
 
 arrIncludes.autoRemove = (val: any) => testFalsey(val) || !val?.length
@@ -46,7 +46,7 @@ const arrIncludesAll: FilterFn<any> = (
   columnId: string,
   filterValue: unknown[]
 ) => {
-  return !filterValue.some(val => !row.getValue(columnId).includes(val))
+  return !filterValue.some(val => !row.getValue(columnId)?.includes(val))
 }
 
 arrIncludesAll.autoRemove = (val: any) => testFalsey(val) || !val?.length
@@ -56,7 +56,7 @@ const arrIncludesSome: FilterFn<any> = (
   columnId: string,
   filterValue: unknown[]
 ) => {
-  return filterValue.some(val => row.getValue(columnId).includes(val))
+  return filterValue.some(val => row.getValue(columnId)?.includes(val))
 }
 
 arrIncludesSome.autoRemove = (val: any) => testFalsey(val) || !val?.length
