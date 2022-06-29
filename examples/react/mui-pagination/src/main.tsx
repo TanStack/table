131e882a7bdf576fb218045857fac9706120566a
Subba Rao Pasupuleti <spasupuleti4@bloomberg.net>
2022-06-29 18:36:17 -0400

    updating latest ract-table

---
+++ examples/react/mui-pagination/src/main.tsx
@@ -0,0 +1,273 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import './index.css'
+import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
+import TableFooter from '@mui/material/TableFooter'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
+import TablePagination from '@mui/material/TablePagination'
+import InputBase from '@mui/material/InputBase'
+import Paper from '@mui/material/Paper'
+
+import {
+  createTable,
+  Column,
+  TableInstance,
+  PaginationState,
+  useTableInstance,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  ColumnDef,
+  OnChangeFn,
+} from '@tanstack/react-table'
+
+import TablePaginationActions from './actions'
+import { makeData, Person } from './makeData'
+
+let table = createTable().setRowType<Person>()
+
+function App() {
+  const rerender = React.useReducer(() => ({}), {})[1]
+
+  const columns = React.useMemo(
+    () => [
+      table.createGroup({
+        header: 'Name',
+        footer: props => props.column.id,
+        columns: [
+          table.createDataColumn('firstName', {
+            cell: info => info.getValue(),
+            footer: props => props.column.id,
+          }),
+          table.createDataColumn(row => row.lastName, {
+            id: 'lastName',
+            cell: info => info.getValue(),
+            header: () => <span>Last Name</span>,
+            footer: props => props.column.id,
+          }),
+        ],
+      }),
+      table.createGroup({
+        header: 'Info',
+        footer: props => props.column.id,
+        columns: [
+          table.createDataColumn('age', {
+            header: () => 'Age',
+            footer: props => props.column.id,
+          }),
+          table.createGroup({
+            header: 'More Info',
+            columns: [
+              table.createDataColumn('visits', {
+                header: () => <span>Visits</span>,
+                footer: props => props.column.id,
+              }),
+              table.createDataColumn('status', {
+                header: 'Status',
+                footer: props => props.column.id,
+              }),
+              table.createDataColumn('progress', {
+                header: 'Profile Progress',
+                footer: props => props.column.id,
+              }),
+            ],
+          }),
+        ],
+      }),
+    ],
+    []
+  )
+
+  const [data, setData] = React.useState(() => makeData(100000))
+  const refreshData = () => setData(() => makeData(100000))
+
+  const [pagination, setPagination] = React.useState<PaginationState>({
+    pageIndex: 0,
+    pageSize: 10,
+  })
+
+  return (
+    <>
+      <LocalTable
+        {...{
+          data,
+          columns,
+          pagination,
+          setPagination,
+        }}
+      />
+      <hr />
+      <div>
+        <button onClick={() => rerender()}>Force Rerender</button>
+      </div>
+      <div>
+        <button onClick={() => refreshData()}>Refresh Data</button>
+      </div>
+      <pre>{JSON.stringify(pagination, null, 2)}</pre>
+    </>
+  )
+}
+
+function LocalTable({
+  data,
+  columns,
+  pagination,
+  setPagination,
+}: {
+  data: Person[]
+  columns: ColumnDef<typeof table.generics>[]
+  pagination: PaginationState
+  setPagination: OnChangeFn<PaginationState>
+}) {
+  const instance = useTableInstance(table, {
+    data,
+    columns,
+    state: {
+      pagination,
+    },
+    onPaginationChange: setPagination,
+    // Pipeline
+    getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    //
+    debugTable: true,
+  })
+  const { pageSize, pageIndex } = instance.getState().pagination
+
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label='simple table'>
+        <TableHead>
+          {instance.getHeaderGroups().map(headerGroup => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map(header => {
+                return (
+                  <TableCell key={header.id} colSpan={header.colSpan}>
+                    {header.isPlaceholder ? null : (
+                      <div>
+                        {header.renderHeader()}
+                        {header.column.getCanFilter() ? (
+                          <div>
+                            <Filter
+                              column={header.column}
+                              instance={instance}
+                            />
+                          </div>
+                        ) : null}
+                      </div>
+                    )}
+                  </TableCell>
+                )
+              })}
+            </TableRow>
+          ))}
+        </TableHead>
+        <TableBody>
+          {instance.getRowModel().rows.map(row => {
+            return (
+              <TableRow key={row.id}>
+                {row.getVisibleCells().map(cell => {
+                  return (
+                    <TableCell key={cell.id}>{cell.renderCell()}</TableCell>
+                  )
+                })}
+              </TableRow>
+            )
+          })}
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TablePagination
+              rowsPerPageOptions={[
+                5,
+                10,
+                25,
+                { label: 'All', value: data.length },
+              ]}
+              colSpan={3}
+              count={instance.getFilteredRowModel().rows.length}
+              rowsPerPage={pageSize}
+              page={pageIndex}
+              SelectProps={{
+                inputProps: { 'aria-label': 'rows per page' },
+                native: true,
+              }}
+              onPageChange={(e, page) => {
+                instance.setPageIndex(page)
+              }}
+              onRowsPerPageChange={e => {
+                const size = e.target.value ? Number(e.target.value) : 10
+                instance.setPageSize(size)
+              }}
+              ActionsComponent={TablePaginationActions}
+            />
+          </TableRow>
+        </TableFooter>
+      </Table>
+    </TableContainer>
+  )
+}
+function Filter({
+  column,
+  instance,
+}: {
+  column: Column<any>
+  instance: TableInstance<any>
+}) {
+  const firstValue = instance
+    .getPreFilteredRowModel()
+    .flatRows[0]?.getValue(column.id)
+
+  const columnFilterValue = column.getFilterValue()
+
+  return typeof firstValue === 'number' ? (
+    <div className='flex space-x-2'>
+      <InputBase
+        type='number'
+        value={(columnFilterValue as [number, number])?.[0] ?? ''}
+        onChange={e =>
+          column.setFilterValue((old: [number, number]) => [
+            e.target.value,
+            old?.[1],
+          ])
+        }
+        placeholder={`Min`}
+        className='w-24 border shadow rounded'
+      />
+      <InputBase
+        type='number'
+        value={(columnFilterValue as [number, number])?.[1] ?? ''}
+        onChange={e =>
+          column.setFilterValue((old: [number, number]) => [
+            old?.[0],
+            e.target.value,
+          ])
+        }
+        placeholder={`Max`}
+        className='w-24 border shadow rounded'
+        inputProps={{ 'aria-label': 'search' }}
+      />
+    </div>
+  ) : (
+    <InputBase
+      value={(columnFilterValue ?? '') as string}
+      onChange={e => column.setFilterValue(e.target.value)}
+      placeholder={`Search...`}
+      className='w-36 border shadow rounded'
+      inputProps={{ 'aria-label': 'search' }}
+    />
+  )
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
