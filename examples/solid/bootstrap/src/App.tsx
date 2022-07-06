536dd97133b501acacd30a6c294ef0cb8397ea10
Bence Fekete <fekete965@gmail.com>
2022-07-06 22:54:23 +0100

    examples: Add solid-bootstrap example (#4095)

---
+++ examples/solid/bootstrap/src/App.tsx
@@ -0,0 +1,148 @@
+import {
+  flexRender,
+  getCoreRowModel,
+  ColumnDef,
+  createSolidTable,
+} from '@tanstack/solid-table'
+import { createSignal, For } from 'solid-js'
+import { makeData, Person } from './makeData'
+import { Table as BTable } from 'solid-bootstrap'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const columns: ColumnDef<Person>[] = [
+  {
+    header: 'Name',
+    footer: props => props.column.id,
+    columns: [
+      {
+        accessorKey: 'firstName',
+        cell: info => info.getValue(),
+        footer: props => props.column.id,
+      },
+      {
+        accessorFn: row => row.lastName,
+        id: 'lastName',
+        cell: info => info.getValue(),
+        header: () => <span>Last Name</span>,
+        footer: props => props.column.id,
+      },
+    ],
+  },
+  {
+    header: 'Info',
+    footer: props => props.column.id,
+    columns: [
+      {
+        accessorKey: 'age',
+        header: () => 'Age',
+        footer: props => props.column.id,
+      },
+      {
+        header: 'More Info',
+        columns: [
+          {
+            accessorKey: 'visits',
+            header: () => <span>Visits</span>,
+            footer: props => props.column.id,
+          },
+          {
+            accessorKey: 'status',
+            header: 'Status',
+            footer: props => props.column.id,
+          },
+          {
+            accessorKey: 'progress',
+            header: 'Profile Progress',
+            footer: props => props.column.id,
+          },
+        ],
+      },
+    ],
+  },
+]
+
+function App() {
+  const [data, setData] = createSignal(makeData(10))
+  const rerender = () => setData(makeData(10))
+
+  const table = createSolidTable({
+    get data() {
+      return data()
+    },
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  })
+
+  return (
+    <div className="p-2">
+      <BTable striped bordered hover responsive size="sm">
+        <thead>
+          <For each={table.getHeaderGroups()}>
+            {headerGroup => (
+              <tr>
+                <For each={headerGroup.headers}>
+                  {header => (
+                    <th colSpan={header.colSpan}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </th>
+                  )}
+                </For>
+              </tr>
+            )}
+          </For>
+        </thead>
+        <tbody>
+          <For each={table.getRowModel().rows}>
+            {row => (
+              <tr>
+                <For each={row.getVisibleCells()}>
+                  {cell => (
+                    <td>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </td>
+                  )}
+                </For>
+              </tr>
+            )}
+          </For>
+        </tbody>
+        <tfoot>
+          <For each={table.getFooterGroups()}>
+            {footerGroup => (
+              <tr>
+                <For each={footerGroup.headers}>
+                  {header => (
+                    <th colSpan={header.colSpan}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.footer,
+                            header.getContext()
+                          )}
+                    </th>
+                  )}
+                </For>
+              </tr>
+            )}
+          </For>
+        </tfoot>
+      </BTable>
+      <div className="h-4">
+        <button onClick={() => rerender()} className="border p-2">
+          Rerender
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default App
