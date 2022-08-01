045b8ae05c0d258c37afa04f1f605328cf2c05fd
Bence Fekete <fekete965@gmail.com>
2022-08-01 22:36:14 +0100

    Add Filtering, Sorting and Pagination - 3. Add Filter, Sorting and Pagination to the table

--- examples/react/kitchen-sink/src/App.tsx
+++ examples/react/kitchen-sink/src/App.tsx
@@ -1,17 +1,28 @@
 import {
-  ColumnDef,
+  ColumnFiltersState,
   flexRender,
   getCoreRowModel,
+  getFacetedMinMaxValues,
+  getFacetedRowModel,
+  getFacetedUniqueValues,
   getFilteredRowModel,
   getPaginationRowModel,
+  getSortedRowModel,
   useReactTable,
 } from '@tanstack/react-table'
 import React from 'react'
-import { Person, makeData } from './makeData'
+import { makeData } from './makeData'
 
 import styled from '@emotion/styled'
 import { useSkipper } from './hooks'
-import { columns, defaultColumn, getTableMeta, TableMeta } from './tableModels'
+import {
+  columns,
+  defaultColumn,
+  fuzzyFilter,
+  getTableMeta,
+} from './tableModels'
+import DebouncedInput from './DebouncedInput'
+import Filter from './Filter'
 
 const Styles = styled.div`
   padding: 1rem;
@@ -56,8 +67,15 @@ const Styles = styled.div`
 `
 
 export const App = () => {
+  const rerender = React.useReducer(() => ({}), {})[1]
+
   const [data, setData] = React.useState(makeData(1000))
   const refreshData = () => setData(makeData(1000))
+
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    []
+  )
+  const [globalFilter, setGlobalFilter] = React.useState('')
 
   const [autoResetPageIndex, skipAutoResetPageIndex] = useSkipper()
 
@@ -68,26 +86,76 @@ export const App = () => {
     getCoreRowModel: getCoreRowModel(),
     getFilteredRowModel: getFilteredRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFacetedRowModel: getFacetedRowModel(),
+    getFacetedUniqueValues: getFacetedUniqueValues(),
+    getFacetedMinMaxValues: getFacetedMinMaxValues(),
+    onColumnFiltersChange: setColumnFilters,
+    onGlobalFilterChange: setGlobalFilter,
+    globalFilterFn: fuzzyFilter,
     autoResetPageIndex,
     // Provide our updateData function to our table meta
     meta: getTableMeta(setData, skipAutoResetPageIndex),
+    state: {
+      columnFilters,
+      globalFilter,
+    },
     debugTable: true,
+    debugHeaders: true,
+    debugColumns: false,
   })
+
+  React.useEffect(() => {
+    if (table.getState().columnFilters[0]?.id === 'fullName') {
+      if (table.getState().sorting[0]?.id !== 'fullName') {
+        table.setSorting([{ id: 'fullName', desc: false }])
+      }
+    }
+  }, [table.getState().columnFilters[0]?.id])
 
   return (
     <Styles>
+      <div className="p-2">
+        <DebouncedInput
+          value={globalFilter ?? ''}
+          onChange={value => setGlobalFilter(String(value))}
+          className="p-2 font-lg shadow border border-block"
+          placeholder="Search all columns..."
+        />
+      </div>
+      <div className="p-2" />
       <table>
         <thead>
           {table.getHeaderGroups().map(headerGroup => (
             <tr key={headerGroup.id}>
               {headerGroup.headers.map(header => (
                 <th key={header.id} colSpan={header.colSpan}>
-                  {header.isPlaceholder
-                    ? null
-                    : flexRender(
-                        header.column.columnDef.header,
-                        header.getContext()
-                      )}
+                  {header.isPlaceholder ? null : (
+                    <>
+                      <div
+                        {...{
+                          className: header.column.getCanSort()
+                            ? 'cursor-pointer select-none'
+                            : '',
+                          onClick: header.column.getToggleSortingHandler(),
+                        }}
+                      >
+                        {flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                        {{
+                          asc: ' 🔼',
+                          desc: ' 🔽',
+                        }[header.column.getIsSorted() as string] ?? null}
+                      </div>
+                      {header.column.getCanFilter() ? (
+                        <div>
+                          <Filter column={header.column} table={table} />
+                        </div>
+                      ) : null}
+                    </>
+                  )}
                 </th>
               ))}
             </tr>
@@ -121,7 +189,76 @@ export const App = () => {
           ))}
         </tfoot>
       </table>
-      <button onClick={refreshData}>Refresh Data</button>
+      <div className="h-2" />
+      <div className="flex items-center gap-2">
+        <button
+          className="border rounded p-1"
+          onClick={() => table.setPageIndex(0)}
+          disabled={!table.getCanPreviousPage()}
+        >
+          {'<<'}
+        </button>
+        <button
+          className="border rounded p-1"
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          {'<'}
+        </button>
+        <button
+          className="border rounded p-1"
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          {'>'}
+        </button>
+        <button
+          className="border rounded p-1"
+          onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+          disabled={!table.getCanNextPage()}
+        >
+          {'>>'}
+        </button>
+        <span className="flex items-center gap-1">
+          <div>Page</div>
+          <strong>
+            {table.getState().pagination.pageIndex + 1} of{' '}
+            {table.getPageCount()}
+          </strong>
+        </span>
+        <span className="flex items-center gap-1">
+          | Go to page:
+          <input
+            type="number"
+            defaultValue={table.getState().pagination.pageIndex + 1}
+            onChange={e => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0
+              table.setPageIndex(page)
+            }}
+            className="border p-1 rounded w-16"
+          />
+        </span>
+        <select
+          value={table.getState().pagination.pageSize}
+          onChange={e => {
+            table.setPageSize(Number(e.target.value))
+          }}
+        >
+          {[10, 20, 30, 40, 50].map(pageSize => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div>{table.getPrePaginationRowModel().rows.length} Rows</div>
+      <div>
+        <button onClick={rerender}>Force Rerender</button>
+      </div>
+      <div>
+        <button onClick={refreshData}>Refresh Data</button>
+      </div>
+      <pre>{JSON.stringify(table.getState(), null, 2)}</pre>
     </Styles>
   )
 }
