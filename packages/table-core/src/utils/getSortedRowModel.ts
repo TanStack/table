6bafe28ec0efadc371da7dbac090e7e03d2dfa98
Kevin Vandy <kevinvandy656@gmail.com>
2022-10-28 17:49:43 -0500

    use spread instead of slice

--- packages/table-core/src/utils/getSortedRowModel.ts
+++ packages/table-core/src/utils/getSortedRowModel.ts
@@ -44,7 +44,7 @@ export function getSortedRowModel<TData extends RowData>(): (
         const sortData = (rows: Row<TData>[]) => {
           // This will also perform a stable sorting using the row index
           // if needed.
-          const sortedData = rows.slice()
+          const sortedData = [...rows]
 
           sortedData.sort((rowA, rowB) => {
             for (let i = 0; i < availableSorting.length; i += 1) {
