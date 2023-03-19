edc97879e278b1c3d639b7fe8f0ee0278fa02701
Zack DeRose <zack.derose@gmail.com>
2023-03-18 17:25:17 -0700

    updating vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,12 +2,10 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-      name: 'tanstack-table',
-      watch: false,
-      setupFiles: ['test-setup.ts'],
+    watch: false,
+    setupFiles: ['test-setup.ts'],
     environment: 'jsdom',
     globals: true,
-    coverage: { provider: 'istanbul' },
     dir: 'packages'
   }
 })
