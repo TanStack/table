e7608ca7a2841ab0402a3b879e05461655708ee1
Kevin Van Cott <kevinvandy656@gmail.com>
2023-09-14 11:07:33 -0500

    chore: upgrade faker and typescript (#5070)

--- examples/vue/pagination/src/makeData.ts
+++ examples/vue/pagination/src/makeData.ts
@@ -20,11 +20,11 @@ const range = (len: number) => {
 
 const newPerson = (): Person => {
   return {
-    firstName: faker.name.firstName(),
-    lastName: faker.name.lastName(),
-    age: faker.datatype.number(40),
-    visits: faker.datatype.number(1000),
-    progress: faker.datatype.number(100),
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+    age: faker.number.int(40),
+    visits: faker.number.int(1000),
+    progress: faker.number.int(100),
     status: faker.helpers.shuffle<Person["status"]>([
       "relationship",
       "complicated",
