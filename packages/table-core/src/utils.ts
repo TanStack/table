51214a88efc51b52d01d9605cb0ba0599ea40ddc
Tanner Linsley <tannerlinsley@gmail.com>
2023-09-15 10:47:50 -0600

    fix: limit deep keys even more

--- packages/table-core/src/utils.ts
+++ packages/table-core/src/utils.ts
@@ -43,16 +43,16 @@ type AllowedIndexes<
   ? AllowedIndexes<Tail, Keys | Tail['length']>
   : Keys
 
-export type DeepKeys<T, TDepth extends any[] = []> = TDepth['length'] extends 10
+export type DeepKeys<T, TDepth extends any[] = []> = TDepth['length'] extends 5
   ? never
   : unknown extends T
-  ? keyof T
+  ? string
   : object extends T
   ? string
   : T extends readonly any[] & IsTuple<T>
   ? AllowedIndexes<T> | DeepKeysPrefix<T, AllowedIndexes<T>, TDepth>
   : T extends any[]
-  ? never & 'Dynamic length array indexing is not supported'
+  ? DeepKeys<T[number], [...TDepth, any]>
   : T extends Date
   ? never
   : T extends object
