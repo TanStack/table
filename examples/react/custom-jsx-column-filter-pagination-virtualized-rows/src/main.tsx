8876b5cc6a2e75b21c512669305d4890e91d568b
Iyed.Chafroud <iyed.chafroud@aureskonnect.com>
2023-10-03 18:04:27 +0100

    exemples: add `exemple` with `custom jsx column` and `pagination `and `virtualized rows` and `filters`

---
+++ examples/react/custom-jsx-column-filter-pagination-virtualized-rows/src/main.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
