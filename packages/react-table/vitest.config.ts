3a2d04950a7e92d6dbf2e2062545f54fc633e733
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2023-12-28 09:56:31 +1100

    Add vitest alias

--- packages/react-table/vitest.config.ts
+++ packages/react-table/vitest.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vitest/config'
+import { resolve } from 'node:path'
 
 export default defineConfig({
   test: {
@@ -7,5 +8,10 @@ export default defineConfig({
     environment: 'jsdom',
     globals: true,
     dir: '__tests__',
+  },
+  resolve: {
+    alias: {
+      '@tanstack/table-core': resolve(__dirname, '../table-core/src'),
+    },
   },
 })
