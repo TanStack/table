174828b7446f78b7444d7dd76f6e21bee1bf7476
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2024-01-05 21:33:40 +1100

    Remove try/catch

--- scripts/publish.js
+++ scripts/publish.js
@@ -3,16 +3,11 @@
 import { publish } from '@tanstack/config/publish'
 import { branchConfigs, packages, rootDir } from './config.js'
 
-try {
-  await publish({
-    branchConfigs,
-    packages,
-    rootDir,
-    branch: process.env.BRANCH,
-    tag: process.env.TAG,
-    ghToken: process.env.GH_TOKEN,
-  })
-} catch (error) {
-  console.error(error)
-  process.exit(1)
-}
+await publish({
+  branchConfigs,
+  packages,
+  rootDir,
+  branch: process.env.BRANCH,
+  tag: process.env.TAG,
+  ghToken: process.env.GH_TOKEN,
+})
