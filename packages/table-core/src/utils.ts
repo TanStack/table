046c133fdc04ea2a227214432ba14fb6b24fda38
Jakub Różbicki <jakub.rozbicki@zaven.pl>
2024-01-18 00:51:30 +0100

    fix: improve ts support when data model has only optional keys

--- packages/table-core/src/utils.ts
+++ packages/table-core/src/utils.ts
@@ -48,7 +48,7 @@ export type DeepKeys<T, TDepth extends any[] = []> = TDepth['length'] extends 5
   : unknown extends T
     ? string
     : object extends T
-      ? string
+      ? (keyof T & string)
       : T extends readonly any[] & IsTuple<T>
         ? AllowedIndexes<T> | DeepKeysPrefix<T, AllowedIndexes<T>, TDepth>
         : T extends any[]
