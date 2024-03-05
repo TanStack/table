3d865e6a18c4c4618c106ddab02b7e87e12ce7ff
Kevin Van Cott <kevinvandy656@gmail.com>
2024-03-05 16:17:09 -0600

    prettier

--- examples/angular/basic/src/app/app.config.ts
+++ examples/angular/basic/src/app/app.config.ts
@@ -1,8 +1,8 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig } from '@angular/core'
+import { provideRouter } from '@angular/router'
 
-import { routes } from './app.routes';
+import { routes } from './app.routes'
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
-};
+  providers: [provideRouter(routes)],
+}
