8516ea7dd7cf513ca7074a1593048f84ba9dfe98
riccardoperra <riccardo.perra@icloud.com>
2024-03-26 22:12:56 +0100

    feat: table proxy detect memoized fns

--- packages/angular-table/src/proxy.ts
+++ packages/angular-table/src/proxy.ts
@@ -1,10 +1,10 @@
 import { computed, isSignal, type Signal, untracked } from '@angular/core'
 import type { Table } from '@tanstack/table-core'
 
-type _TableProxyAccessor<T> = T extends () => infer U ? Signal<U> : never
+type TableProxyAccessor<T> = T extends () => infer U ? Signal<U> : never
 
 type _TableProxy<T extends Table<any>> = {
-  [K in keyof T]: K extends `get${string}` ? _TableProxyAccessor<T[K]> : T[K]
+  [K in keyof T]: K extends `get${string}` ? TableProxyAccessor<T[K]> : T[K]
 } & {
   options: Signal<T['options']>
 }
@@ -13,21 +13,25 @@ export type TableProxy<T> = Signal<Table<T>> & _TableProxy<Table<T>>
 
 // WIP
 export function proxifyTable<T>(tableSignal: Signal<Table<T>>) {
+  const propertyCache: Record<string, any> = {}
+
   const proxyTable = new Proxy(tableSignal, {
     get(target: Signal<Table<T>>, property: keyof Table<T>): any {
+      if (propertyCache[property]) {
+        return propertyCache[property]
+      }
       const untypedTarget = target as any
-      const table = untracked(target)
+      if (untypedTarget[property]) {
+        return untypedTarget[property]
+      }
 
+      const table = untracked(target)
       if (!(property in table)) {
         return untypedTarget[property]
       }
 
-      if (isSignal(untypedTarget[property])) {
-        return untypedTarget[property]
-      }
-
-      // Try to transform all accessors into computed, skipping
-      // handlers since they don't hold any value
+      // Attempt to convert all accessors into computed ones,
+      // excluding handlers as they do not retain any value.
       if (property.startsWith('get') && !property.endsWith('Handler')) {
         const maybeFn = table[property] as Function | never
         if (typeof maybeFn === 'function') {
@@ -39,18 +43,40 @@ export function proxifyTable<T>(tableSignal: Signal<Table<T>>) {
               enumerable: true,
             })
           }
-          // Accessors with one argument could be memoized, e.g. `table.getHeaderGroups`
+
+          // Accessors with one argument could be a memoized fn (e.g. `getHeaderGroups(memoArgs)`)
+          // or a fn with some dependent parameter in their signature (e.g. `getIsSomeRowsPinned(position)`)
+          // TODO: need to check better this, since there are some fns that have an optional argument
           if (maybeFn.length === 1) {
+            let computedSignal: Signal<unknown> | undefined
+            const maybeComputedTrap = new Proxy(maybeFn, {
+              apply(target: any, thisArg: any, argArray: any[]): any {
+                if (argArray.length === 0) {
+                  if (computedSignal) {
+                    return computedSignal()
+                  }
+                  computedSignal = computed(() =>
+                    Reflect.apply(target, thisArg, argArray)
+                  )
+                  // We don't need the proxy anymore, so we'll override the cache value
+                  // in order to use the existing signal in the next change detection cycle
+                  propertyCache[property] = computedSignal
+                  return computedSignal()
+                }
+                return Reflect.apply(target, thisArg, argArray)
+              },
+            })
+
+            propertyCache[property] = maybeComputedTrap
+
             Object.defineProperty(untypedTarget, property, {
-              // TODO: check if this make sense
-              //  Computed could run again but emitted value passes through the `memo` fn
-              value: computed(() => untypedTarget()[property](), { equal: () => false }),
+              value: maybeComputedTrap,
               configurable: true,
               enumerable: true,
             })
           }
 
-          // Accessors with arguments could be impure functions
+          // Accessors with arguments could be impure functions, so we can't memoize the value
           if (maybeFn.length > 1) {
             Object.defineProperty(untypedTarget, property, {
               value: target()[property],
