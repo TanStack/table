2f580472b44855dbb5dfc76a41166dc4c53b919b
Mor Kadosh <mkadosh@paloaltonetworks.com>
2024-05-15 20:37:05 +0300

    docs: add twind and some styling to basic example

--- examples/lit/basic/src/main.ts
+++ examples/lit/basic/src/main.ts
@@ -7,6 +7,10 @@ import {
   getCoreRowModel,
   TableController,
 } from '@tanstack/lit-table'
+import install from '@twind/with-web-components'
+import config from '../twind.config'
+
+const withTwind = install(config)
 
 type Person = {
   firstName: string
@@ -16,33 +20,6 @@ type Person = {
   status: string
   progress: number
 }
-
-const defaultData: Person[] = [
-  {
-    firstName: 'tanner',
-    lastName: 'linsley',
-    age: 24,
-    visits: 100,
-    status: 'In Relationship',
-    progress: 50,
-  },
-  {
-    firstName: 'tandy',
-    lastName: 'miller',
-    age: 40,
-    visits: 40,
-    status: 'Single',
-    progress: 80,
-  },
-  {
-    firstName: 'joe',
-    lastName: 'dirte',
-    age: 45,
-    visits: 20,
-    status: 'Complicated',
-    progress: 10,
-  },
-]
 
 const columnHelper = createColumnHelper<Person>()
 
@@ -112,6 +89,7 @@ const data: Person[] = [
 ]
 
 @customElement('lit-table-example')
+@withTwind
 class LitTableExample extends LitElement {
   private tableController = new TableController<Person>(this)
 
@@ -125,69 +103,71 @@ class LitTableExample extends LitElement {
     return html`
       <table>
         <thead>
-          ${repeat(
-            table.getHeaderGroups(),
-            headerGroup => headerGroup.id,
-            headerGroup =>
-              html`${repeat(
-                headerGroup.headers,
-                header => header.id,
-                header =>
-                  html` <th>
+        ${repeat(
+          table.getHeaderGroups(),
+          headerGroup => headerGroup.id,
+          headerGroup =>
+            html`${repeat(
+              headerGroup.headers,
+              header => header.id,
+              header =>
+                html`
+                  <th>
                     ${header.isPlaceholder
                       ? null
                       : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
-                        )}
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
                   </th>`
-              )}`
-          )}
+            )}`
+        )}
         </thead>
         <tbody>
-          ${repeat(
-            table.getRowModel().rows,
-            row => row.id,
-            row => html`
-              <tr>
-                ${repeat(
-                  row.getVisibleCells(),
-                  cell => cell.id,
-                  cell =>
-                    html`<td>
+        ${repeat(
+          table.getRowModel().rows,
+          row => row.id,
+          row => html`
+            <tr>
+              ${repeat(
+                row.getVisibleCells(),
+                cell => cell.id,
+                cell =>
+                  html`
+                    <td>
                       ${flexRender(
                         cell.column.columnDef.cell,
                         cell.getContext()
                       )}
                     </td>`
-                )}
-              </tr>
-            `
-          )}
+              )}
+            </tr>
+          `
+        )}
         </tbody>
         <tfoot>
-          ${repeat(
-            table.getFooterGroups(),
-            footerGroup => footerGroup.id,
-            footerGroup => html`
-              <tr>
-                ${repeat(
-                  footerGroup.headers,
-                  header => header.id,
-                  header => html`
-                    <th>
-                      ${header.isPlaceholder
-                        ? null
-                        : flexRender(
-                            header.column.columnDef.footer,
-                            header.getContext()
-                          )}
-                    </th>
-                  `
-                )}
-              </tr>
-            `
-          )}
+        ${repeat(
+          table.getFooterGroups(),
+          footerGroup => footerGroup.id,
+          footerGroup => html`
+            <tr>
+              ${repeat(
+                footerGroup.headers,
+                header => header.id,
+                header => html`
+                  <th>
+                    ${header.isPlaceholder
+                      ? null
+                      : flexRender(
+                        header.column.columnDef.footer,
+                        header.getContext()
+                      )}
+                  </th>
+                `
+              )}
+            </tr>
+          `
+        )}
         </tfoot>
       </table>
       <style>
@@ -219,6 +199,7 @@ class LitTableExample extends LitElement {
         tfoot th {
           font-weight: normal;
         }
+
       </style>
     `
   }
