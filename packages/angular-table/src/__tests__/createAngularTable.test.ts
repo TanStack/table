0f4e820a86f30024db41afae8facd7aa0e4f43b6
riccardoperra <riccardo.perra@icloud.com>
2024-05-19 12:49:55 +0200

    add test case

--- packages/angular-table/src/__tests__/createAngularTable.test.ts
+++ packages/angular-table/src/__tests__/createAngularTable.test.ts
@@ -5,32 +5,54 @@ import {
   getCoreRowModel,
   type Table,
 } from '../index'
-import { isSignal, signal, untracked } from '@angular/core'
+import { Component, input, isSignal, signal, untracked } from '@angular/core'
+import { TestBed } from '@angular/core/testing'
+import { setSignalInputs } from './test-utils'
 
 describe('createAngularTable', () => {
-  type Data = { id: string; title: string }
+  test('should render with required signal inputs', () => {
+    @Component({
+      selector: 'app-fake',
+      template: ``,
+      standalone: true,
+    })
+    class FakeComponent {
+      data = input.required<any[]>()
 
-  const data = signal<Data[]>([{ id: '1', title: 'Title' }])
+      table = createAngularTable(() => ({
+        data: this.data(),
+        columns: [],
+        getCoreRowModel: getCoreRowModel(),
+      }))
+    }
 
-  const columns: ColumnDef<Data>[] = [
-    { id: 'id', header: 'Id', cell: context => context.getValue() },
-    { id: 'title', header: 'Title', cell: context => context.getValue() },
-  ]
+    const fixture = TestBed.createComponent(FakeComponent)
+    setSignalInputs(fixture.componentInstance, {
+      data: [],
+    })
 
-  const table = createAngularTable(() => ({
-    data: data(),
-    columns: columns,
-    getCoreRowModel: getCoreRowModel(),
-    getRowId: row => row.id,
-  }))
-
-  const tablePropertyKeys = Object.keys(table())
-
-  it('should be a signal', () => {
-    expect(isSignal(table)).toEqual(true)
+    fixture.detectChanges()
   })
 
   describe('proxy trap', () => {
+    type Data = { id: string; title: string }
+    const data = signal<Data[]>([{ id: '1', title: 'Title' }])
+    const columns: ColumnDef<Data>[] = [
+      { id: 'id', header: 'Id', cell: context => context.getValue() },
+      { id: 'title', header: 'Title', cell: context => context.getValue() },
+    ]
+    const table = createAngularTable(() => ({
+      data: data(),
+      columns: columns,
+      getCoreRowModel: getCoreRowModel(),
+      getRowId: row => row.id,
+    }))
+    const tablePropertyKeys = Object.keys(table())
+
+    test('table must be a signal', () => {
+      expect(isSignal(table)).toEqual(true)
+    })
+
     test('supports "in" operator', () => {
       expect('getCoreRowModel' in table).toBe(true)
       expect('options' in table).toBe(true)
