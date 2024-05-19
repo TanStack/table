be0b8a206e26ff8c3fe874b893039c3b9beb4054
riccardoperra <riccardo.perra@icloud.com>
2024-05-19 11:55:21 +0200

    update infra

--- packages/angular-table/vitest.config.ts
+++ packages/angular-table/vitest.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => ({
   test: {
     name: packageJson.name,
     globals: true,
-    setupFiles: ['src/test-setup.ts'],
+    setupFiles: ['src/__tests__/test-setup.ts'],
     environment: 'jsdom',
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
   },
