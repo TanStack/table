7c0bf98ee3686474f37451d3345aad146d738516
riccardoperra <riccardo.perra@icloud.com>
2024-05-19 12:59:13 +0200

    update tsconfig

--- packages/angular-table/vitest.config.ts
+++ packages/angular-table/vitest.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vitest/config'
 import packageJson from './package.json'
-import angular from '@analogjs/vite-plugin-angular'
 
 export default defineConfig(({ mode }) => ({
   test: {
