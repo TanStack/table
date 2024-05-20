9b8a0cabf124e4abaf6d5a7dde76b34e50f9c8f8
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2024-05-20 18:48:43 +1000

    Migrate qwik-table

---
+++ packages/qwik-table/vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig, mergeConfig } from 'vitest/config'
+import { tanstackBuildConfig } from '@tanstack/config/build'
+
+const config = defineConfig({})
+
+export default mergeConfig(
+  config,
+  tanstackBuildConfig({
+    entry: './src/index.tsx',
+    srcDir: './src',
+  })
+)
