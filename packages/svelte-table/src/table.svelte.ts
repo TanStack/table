d3d9101afe1be901d1315411ef774cebb9283dc3
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2024-06-12 19:08:00 +1000

    Fix svelte update

--- packages/svelte-table/src/table.svelte.ts
+++ packages/svelte-table/src/table.svelte.ts
@@ -80,20 +80,16 @@ export function createSvelteTable<TData extends RowData>(
  * Merges objects together while keeping their getters alive.
  * Taken from SolidJS: {@link https://github.com/solidjs/solid/blob/24abc825c0996fd2bc8c1de1491efe9a7e743aff/packages/solid/src/server/rendering.ts#L82-L115}
  * */
-export function mergeObjects<T>(source: T): T
-export function mergeObjects<T, U>(source: T, source1: U): T & U
-export function mergeObjects<T, U, V>(
-  source: T,
-  source1: U,
-  source2: V
-): T & U & V
-export function mergeObjects<T, U, V, W>(
+function mergeObjects<T>(source: T): T
+function mergeObjects<T, U>(source: T, source1: U): T & U
+function mergeObjects<T, U, V>(source: T, source1: U, source2: V): T & U & V
+function mergeObjects<T, U, V, W>(
   source: T,
   source1: U,
   source2: V,
   source3: W
 ): T & U & V & W
-export function mergeObjects(...sources: any): any {
+function mergeObjects(...sources: any): any {
   const target = {}
   for (let i = 0; i < sources.length; i++) {
     let source = sources[i]
