3719323d9f032d55e1d0b2991736979887e36e00
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2024-07-28 09:25:21 +1000

    chore(typedoc): add reference doc generation

---
+++ scripts/generateDocs.js
@@ -0,0 +1,26 @@
+import { resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { generateReferenceDocs } from '@tanstack/config/typedoc'
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
+
+/** @type {import('@tanstack/config/typedoc').Package[]} */
+const packages = [
+  {
+    name: 'table-core',
+    entryPoints: [resolve(__dirname, '../packages/table-core/src/index.ts')],
+    tsconfig: resolve(__dirname, '../packages/table-core/tsconfig.json'),
+    outputDir: resolve(__dirname, '../docs/reference'),
+  },
+  {
+    name: 'react-table',
+    entryPoints: [resolve(__dirname, '../packages/react-table/src/index.tsx')],
+    tsconfig: resolve(__dirname, '../packages/react-table/tsconfig.json'),
+    outputDir: resolve(__dirname, '../docs/framework/react/reference'),
+    exclude: ['packages/table-core/**/*'],
+  },
+]
+
+await generateReferenceDocs({ packages })
+
+process.exit(0)
