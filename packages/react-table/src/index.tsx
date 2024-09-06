266a7a8f8f87125a67e4e48a04930b6ae9fa1447
vismay7 <vismaypatel252@gmail.com>
2024-09-06 12:19:06 +0530

    chore: useRef instead of useState

--- packages/react-table/src/index.tsx
+++ packages/react-table/src/index.tsx
@@ -2,9 +2,9 @@ import * as React from 'react'
 export * from '@tanstack/table-core'
 
 import {
+  RowData,
   TableOptions,
   TableOptionsResolved,
-  RowData,
   createTable,
 } from '@tanstack/table-core'
 
@@ -65,13 +65,11 @@ export function useReactTable<TData extends RowData>(
     ...options,
   }
 
-  // Create a new table and store it in state
-  const [tableRef] = React.useState(() => ({
-    current: createTable<TData>(resolvedOptions),
-  }))
+  // Create a new table and store it in ref
+  const tableRef = React.useRef(createTable<TData>(resolvedOptions))
 
   // By default, manage table state here using the table's initial state
-  const [state, setState] = React.useState(() => tableRef.current.initialState)
+  const [state, setState] = React.useState(() => tableRef.current?.initialState)
 
   // Compose the default state above with any user state. This will allow the user
   // to only control a subset of the state if desired.
