82ee0994d2f16970f627300f3b82f64de7755c8d
Joel 'Aaron' Cohen <acohen@deepsig.ai>
2024-11-04 16:54:31 -0500

    Add failing test showing row is selected with no selectable subRows

--- packages/table-core/tests/RowSelection.test.ts
+++ packages/table-core/tests/RowSelection.test.ts
@@ -201,6 +201,34 @@ describe('RowSelection', () => {
       expect(result).toEqual(false)
     })
 
+    it('should return false if no sub-rows are selectable', () => {
+      const data = makeData(3, 2)
+      const columns = generateColumns(data)
+
+      const table = createTable<Person>({
+        enableRowSelection: false,
+        onStateChange() {},
+        renderFallbackValue: '',
+        data,
+        getSubRows: row => row.subRows,
+        state: {
+          rowSelection: {},
+        },
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+      })
+
+      const firstRow = table.getCoreRowModel().rows[0]
+
+      const result = RowSelection.isSubRowSelected(
+        firstRow,
+        table.getState().rowSelection,
+        table
+      )
+
+      expect(result).toEqual(false)
+    })
+
     it('should return some if some sub-rows are selected', () => {
       const data = makeData(3, 2)
       const columns = generateColumns(data)
