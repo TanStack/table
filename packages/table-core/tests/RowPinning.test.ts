fb96ac83cf414eb1ccd463c65b79f8fdde9fbd47
will short <willofmetal85@gmail.com>
2024-12-18 18:34:32 -0600

    Fix/tanstack v9 feature test fixes (#5837)
    * fix: updated tsconfig to include test files
    
    * fix: getGroupRowModel tests
    
    * fix: RowPinning.test.ts
    
    * fix: RowSelection tests
    
    * fix: removed uneeded change

--- packages/table-core/tests/RowPinning.test.ts
+++ packages/table-core/tests/RowPinning.test.ts
@@ -1,25 +1,27 @@
-import { describe, expect, it } from 'vitest'
+import { beforeEach, describe, expect, it } from 'vitest'
 import {
-  ColumnDef,
-  createColumnHelper,
   constructTable,
-  createCoreRowModel,
+  coreFeatures,
+  createColumnHelper,
   createPaginatedRowModel,
-  rowPinningFeature,
   rowPaginationFeature,
+  rowPinningFeature,
 } from '../src'
-import { makeData, Person } from './makeTestData'
+import { makeData } from './makeTestData'
+import type { ColumnDef } from '../src'
+import type { Person } from './makeTestData'
 
 type personKeys = keyof Person
-type PersonColumn = ColumnDef<
-  any,
-  Person,
-  string | number | Person[] | undefined
->
+type PersonColumn = ColumnDef<any, Person, any>
 
-function generateColumns(people: Person[]): PersonColumn[] {
+function generateColumns(people: Array<Person>): Array<PersonColumn> {
   const columnHelper = createColumnHelper<any, Person>()
   const person = people[0]
+
+  if (!person) {
+    return []
+  }
+
   return Object.keys(person).map((key) => {
     const typedKey = key as personKeys
     return columnHelper.accessor(typedKey, { id: typedKey })
@@ -27,14 +29,22 @@ function generateColumns(people: Person[]): PersonColumn[] {
 }
 
 describe('rowPinningFeature', () => {
+  let data: Array<Person>
+  let columns: Array<PersonColumn>
+  beforeEach(() => {
+    data = makeData(10)
+    columns = generateColumns(data)
+  })
+
   describe('constructTable', () => {
     describe('getTopRows', () => {
       it('should return pinned rows when keepPinnedRows is true rows are visible', () => {
-        const data = makeData(10)
-        const columns = generateColumns(data)
-
         const table = constructTable<any, Person>({
-          _features: { rowPinningFeature, rowPaginationFeature },
+          _features: {
+            rowPinningFeature,
+            rowPaginationFeature,
+            ...coreFeatures,
+          },
           _rowModels: {
             paginatedRowModel: createPaginatedRowModel(),
           },
@@ -46,7 +56,7 @@ describe('rowPinningFeature', () => {
           state: {
             pagination: {
               pageSize: 5,
-              pageIndex: 0, //pinned rows will be on page 0
+              pageIndex: 0, // pinned rows will be on page 0
             },
             rowPinning: {
               bottom: [],
@@ -59,15 +69,16 @@ describe('rowPinningFeature', () => {
         const result = table.getTopRows()
 
         expect(result.length).toBe(2)
-        expect(result[0].id).toBe('0')
-        expect(result[1].id).toBe('1')
+        expect(result[0]?.id).toBe('0')
+        expect(result[1]?.id).toBe('1')
       })
       it('should return pinned rows when keepPinnedRows is true rows are not visible', () => {
-        const data = makeData(10)
-        const columns = generateColumns(data)
-
         const table = constructTable<any, Person>({
-          _features: { rowPinningFeature, rowPaginationFeature },
+          _features: {
+            rowPinningFeature,
+            rowPaginationFeature,
+            ...coreFeatures,
+          },
           _rowModels: {
             paginatedRowModel: createPaginatedRowModel(),
           },
@@ -79,7 +90,7 @@ describe('rowPinningFeature', () => {
           state: {
             pagination: {
               pageSize: 5,
-              pageIndex: 1, //pinned rows will be on page 0
+              pageIndex: 1, // pinned rows will be on page 0
             },
             rowPinning: {
               bottom: [],
@@ -92,15 +103,16 @@ describe('rowPinningFeature', () => {
         const result = table.getTopRows()
 
         expect(result.length).toBe(2)
-        expect(result[0].id).toBe('0')
-        expect(result[1].id).toBe('1')
+        expect(result[0]?.id).toBe('0')
+        expect(result[1]?.id).toBe('1')
       })
       it('should return pinned rows when keepPinnedRows is false rows are visible', () => {
-        const data = makeData(10)
-        const columns = generateColumns(data)
-
         const table = constructTable<any, Person>({
-          _features: { rowPinningFeature, rowPaginationFeature },
+          _features: {
+            rowPinningFeature,
+            rowPaginationFeature,
+            ...coreFeatures,
+          },
           _rowModels: {
             paginatedRowModel: createPaginatedRowModel(),
           },
@@ -112,7 +124,7 @@ describe('rowPinningFeature', () => {
           state: {
             pagination: {
               pageSize: 5,
-              pageIndex: 0, //pinned rows will be on page 0
+              pageIndex: 0, // pinned rows will be on page 0
             },
             rowPinning: {
               bottom: [],
@@ -125,15 +137,16 @@ describe('rowPinningFeature', () => {
         const result = table.getTopRows()
 
         expect(result.length).toBe(2)
-        expect(result[0].id).toBe('0')
-        expect(result[1].id).toBe('1')
+        expect(result[0]?.id).toBe('0')
+        expect(result[1]?.id).toBe('1')
       })
       it('should not return pinned rows when keepPinnedRows is false and rows are not visible', () => {
-        const data = makeData(10)
-        const columns = generateColumns(data)
-
         const table = constructTable<any, Person>({
-          _features: { rowPinningFeature, rowPaginationFeature },
+          _features: {
+            rowPinningFeature,
+            rowPaginationFeature,
+            ...coreFeatures,
+          },
           _rowModels: {
             paginatedRowModel: createPaginatedRowModel(),
           },
@@ -145,7 +158,7 @@ describe('rowPinningFeature', () => {
           state: {
             pagination: {
               pageSize: 5,
-              pageIndex: 1, //pinned rows will be on page 0, but this is page 1
+              pageIndex: 1, // pinned rows will be on page 0, but this is page 1
             },
             rowPinning: {
               bottom: [],
@@ -161,11 +174,12 @@ describe('rowPinningFeature', () => {
       })
 
       it('should return correct top rows', () => {
-        const data = makeData(10)
-        const columns = generateColumns(data)
-
         const table = constructTable<any, Person>({
-          _features: { rowPinningFeature, rowPaginationFeature },
+          _features: {
+            rowPinningFeature,
+            rowPaginationFeature,
+            ...coreFeatures,
+          },
           _rowModels: {
             paginatedRowModel: createPaginatedRowModel(),
           },
@@ -177,7 +191,7 @@ describe('rowPinningFeature', () => {
           state: {
             pagination: {
               pageSize: 5,
-              pageIndex: 0, //pinned rows will be on page 0
+              pageIndex: 0, // pinned rows will be on page 0
             },
             rowPinning: {
               bottom: [],
@@ -190,15 +204,16 @@ describe('rowPinningFeature', () => {
         const result = table.getTopRows()
 
         expect(result.length).toBe(2)
-        expect(result[0].id).toBe('1')
-        expect(result[1].id).toBe('3')
+        expect(result[0]?.id).toBe('1')
+        expect(result[1]?.id).toBe('3')
       })
       it('should return correct bottom rows', () => {
-        const data = makeData(10)
-        const columns = generateColumns(data)
-
         const table = constructTable<any, Person>({
-          _features: { rowPinningFeature, rowPaginationFeature },
+          _features: {
+            rowPinningFeature,
+            rowPaginationFeature,
+            ...coreFeatures,
+          },
           _rowModels: {
             paginatedRowModel: createPaginatedRowModel(),
           },
@@ -210,7 +225,7 @@ describe('rowPinningFeature', () => {
           state: {
             pagination: {
               pageSize: 5,
-              pageIndex: 0, //pinned rows will be on page 0
+              pageIndex: 0, // pinned rows will be on page 0
             },
             rowPinning: {
               bottom: ['1', '3'],
@@ -223,8 +238,8 @@ describe('rowPinningFeature', () => {
         const result = table.getBottomRows()
 
         expect(result.length).toBe(2)
-        expect(result[0].id).toBe('1')
-        expect(result[1].id).toBe('3')
+        expect(result[0]?.id).toBe('1')
+        expect(result[1]?.id).toBe('3')
       })
     })
     describe('getCenterRows', () => {
@@ -233,7 +248,11 @@ describe('rowPinningFeature', () => {
         const columns = generateColumns(data)
 
         const table = constructTable<any, Person>({
-          _features: { rowPinningFeature, rowPaginationFeature },
+          _features: {
+            rowPinningFeature,
+            rowPaginationFeature,
+            ...coreFeatures,
+          },
           _rowModels: {
             paginatedRowModel: createPaginatedRowModel(),
           },
@@ -258,8 +277,8 @@ describe('rowPinningFeature', () => {
         const result = table.getCenterRows()
 
         expect(result.length).toBe(2)
-        expect(result[0].id).toBe('0') // 0 and 5 are the only rows not pinned
-        expect(result[1].id).toBe('5')
+        expect(result[0]?.id).toBe('0') // 0 and 5 are the only rows not pinned
+        expect(result[1]?.id).toBe('5')
       })
     })
   })
--- packages/table-core/tests/getGroupedRowModel.test.ts
+++ packages/table-core/tests/getGroupedRowModel.test.ts
@@ -1,24 +1,27 @@
 import { describe, expect, it } from 'vitest'
 import {
-  constructTable,
-  ColumnDef,
-  createCoreRowModel,
-  createGroupedRowModel,
+  aggregationFns,
   columnGroupingFeature,
+  constructTable,
+  coreFeatures,
+  createGroupedRowModel,
 } from '../src'
 import { createColumnHelper } from '../src/helpers/columnHelper'
-import { makeData, Person } from './makeTestData'
+import { makeData } from './makeTestData'
+import type { Person } from './makeTestData'
+import type { ColumnDef } from '../src'
 
 type personKeys = keyof Person
-type PersonColumn = ColumnDef<
-  any,
-  Person,
-  string | number | Person[] | undefined
->
+type PersonColumn = ColumnDef<any, Person, any>
 
-function generateColumns(people: Person[]): PersonColumn[] {
+function generateColumns(people: Array<Person>): Array<PersonColumn> {
   const columnHelper = createColumnHelper<any, Person>()
   const person = people[0]
+
+  if (!person) {
+    return []
+  }
+
   return Object.keys(person).map((key) => {
     const typedKey = key as personKeys
     return columnHelper.accessor(typedKey, { id: typedKey })
@@ -37,9 +40,9 @@ describe('#getGroupedRowModel', () => {
     data.forEach((p) => (p.age = 123))
 
     const table = constructTable<any, Person>({
-      _features: { columnGroupingFeature },
+      _features: { columnGroupingFeature, ...coreFeatures },
       _rowModels: {
-        groupedRowModel: createGroupedRowModel(),
+        groupedRowModel: createGroupedRowModel(aggregationFns),
       },
       onStateChange() {},
       renderFallbackValue: '',
@@ -50,12 +53,13 @@ describe('#getGroupedRowModel', () => {
     const groupedById = table.getGroupedRowModel().rowsById
     const end = new Date()
 
-    expect(groupedById['firstName:Fixed'].leafRows.length).toEqual(50000)
+    expect(groupedById['firstName:Fixed']?.getLeafRows().length).toEqual(50002)
     expect(
-      groupedById['firstName:Fixed>lastName:Name'].leafRows.length,
-    ).toEqual(50000)
+      groupedById['firstName:Fixed>lastName:Name']?.getLeafRows().length,
+    ).toEqual(50001)
     expect(
-      groupedById['firstName:Fixed>lastName:Name>age:123'].leafRows.length,
+      groupedById['firstName:Fixed>lastName:Name>age:123']?.getLeafRows()
+        .length,
     ).toEqual(50000)
     expect(end.valueOf() - start.valueOf()).toBeLessThan(5000)
   })
