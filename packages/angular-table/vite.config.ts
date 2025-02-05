fdfca1346068a60f5198b0944610173751e00626
riccardoperra <riccardo.perra@icloud.com>
2025-02-05 20:00:36 +0100

    angular: update vite config to support vitest workspaces

--- packages/angular-table/vite.config.ts
+++ packages/angular-table/vite.config.ts
@@ -1,17 +1,21 @@
+import * as path from 'node:path'
 import { defineConfig } from 'vitest/config'
-import packageJson from './package.json'
 import angular from '@analogjs/vite-plugin-angular'
+import packageJson from './package.json'
 
-const angularPlugin = angular({ tsconfig: 'tsconfig.test.json', jit: true })
+const tsconfigPath = path.join(import.meta.dirname, 'tsconfig.test.json')
+const testDirPath = path.join(import.meta.dirname, 'tests')
+const angularPlugin = angular({ tsconfig: tsconfigPath, jit: true })
 
 export default defineConfig({
   plugins: [angularPlugin],
   test: {
     name: packageJson.name,
     watch: false,
+    dir: testDirPath,
     pool: 'threads',
     environment: 'jsdom',
-    setupFiles: ['./tests/test-setup.ts'],
+    setupFiles: [path.join(testDirPath, 'test-setup.ts')],
     globals: true,
     reporters: 'default',
     disableConsoleIntercept: true,
