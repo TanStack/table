f0b352f0c65e99773968ed9072e3fbbcf6f3931e
Pascal Küsgen <pascalkuesgen@gmail.com>
2025-06-17 08:31:08 +0200

    docs: remove unused footer colum def

--- examples/react/expanding/src/main.tsx
+++ examples/react/expanding/src/main.tsx
@@ -77,34 +77,28 @@ function App() {
             </div>
           </div>
         ),
-        footer: props => props.column.id,
       },
       {
         accessorFn: row => row.lastName,
         id: 'lastName',
         cell: info => info.getValue(),
         header: () => <span>Last Name</span>,
-        footer: props => props.column.id,
       },
       {
         accessorKey: 'age',
         header: () => 'Age',
-        footer: props => props.column.id,
       },
       {
         accessorKey: 'visits',
         header: () => <span>Visits</span>,
-        footer: props => props.column.id,
       },
       {
         accessorKey: 'status',
         header: 'Status',
-        footer: props => props.column.id,
       },
       {
         accessorKey: 'progress',
         header: 'Profile Progress',
-        footer: props => props.column.id,
       },
     ],
     []
