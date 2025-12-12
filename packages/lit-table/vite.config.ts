51aca77b3a9784c60f9bd3e08b06aedad4c57b40
Kevin Van Cott <kevinvandy656@gmail.com>
2025-12-12 07:20:21 -0600

    package upgrades

--- packages/lit-table/vite.config.ts
+++ packages/lit-table/vite.config.ts
@@ -6,6 +6,7 @@ const config = defineConfig({})
 export default mergeConfig(
   config,
   tanstackViteConfig({
+    cjs: false,
     entry: './src/index.ts',
     srcDir: './src',
   }),
--- packages/react-table/vite.config.ts
+++ packages/react-table/vite.config.ts
@@ -17,8 +17,5 @@ const config = defineConfig({
 
 export default mergeConfig(
   config,
-  tanstackViteConfig({
-    entry: './src/index.ts',
-    srcDir: './src',
-  }),
+  tanstackViteConfig({ cjs: false, entry: './src/index.ts', srcDir: './src' }),
 )
