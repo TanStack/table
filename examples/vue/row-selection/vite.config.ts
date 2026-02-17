0f33c2609e1a52dfa043eb55bed80c8390b0bda5
Nicholas Ferguson <n.ferguson@f5.com>
2026-02-17 10:44:34 -0800

    Added server.allowed hosts to vite config. Fixes #6175

--- examples/vue/row-selection/vite.config.ts
+++ examples/vue/row-selection/vite.config.ts
@@ -4,5 +4,8 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    allowedHosts: true,
+  },
   plugins: [vue(), vueJsx()],
 })
