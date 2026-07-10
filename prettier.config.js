02129e193cbd186196c2b55c4f9738f55a1193d0
Ryan Tablada <rtablada@auditboard.com>
2026-07-10 10:07:13 -0500

    chore: remove duplicate configs and move prettier to root

--- prettier.config.js
+++ prettier.config.js
@@ -5,7 +5,7 @@ const config = {
   semi: false,
   singleQuote: true,
   trailingComma: 'all',
-  plugins: ['prettier-plugin-svelte'],
+  plugins: ['prettier-plugin-svelte', 'prettier-plugin-ember-template-tag'],
   overrides: [
     { files: '*.svelte', options: { parser: 'svelte' } },
     {
@@ -13,6 +13,12 @@ const config = {
       options: {
         printWidth: 100,
         parser: 'angular',
+      },
+    },
+    {
+      files: '*.{gjs,gts}',
+      options: {
+        templateSingleQuote: false,
       },
     },
   ],
