50f2133167f9b1f96c0908c4c9dfc9dab89ed7f7
Nicholas <technicallynick@gmail.com>
2026-08-02 05:10:14 -0700

    chore: Added server.allowed hosts to vite config. Fixes #6175 (#6176)
    Co-authored-by: Nicholas Ferguson <n.ferguson@f5.com>

--- examples/qwik/row-selection/vite.config.ts
+++ examples/qwik/row-selection/vite.config.ts
@@ -3,6 +3,9 @@ import { qwikVite } from '@builder.io/qwik/optimizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    allowedHosts: true,
+  },
   plugins: [
     qwikVite({
       csr: true,
